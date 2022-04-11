d2136f4ed83e580752af2f495d8bcd155d9267e3
Dem_Emm <Dem_Emm@hotmail.com>
2022-04-11 16:47:51 +0300

    added  data_delete() for specific deletion of a data.

--- Binary_Search_Trees/Source.cpp
+++ Binary_Search_Trees/Source.cpp
@@ -2,7 +2,9 @@
 #include<stdio.h>
 #include<stdbool.h>
 #include<queue>
+
 using namespace std;
+
 struct node {
 	int data;
 	node* right;
@@ -59,13 +61,23 @@ void print_tree_level(node* root) {
 	while (!Q.empty()) {
 		temp = Q.front();
 		int old_level = level.front();
-		if (temp->left != NULL) Q.push(temp->left); level.push(level.front() + 1);
-		if (temp->right != NULL) Q.push(temp->right); level.push(level.front() + 1);
+		if (temp->left != NULL) {
+		Q.push(temp->left);
+		level.push(level.front() + 1);
+	}
+		if (temp->right != NULL){
+			Q.push(temp->right);
+			level.push(level.front() + 1);
+			}
 		printf("%d level %d           ", temp->data, level.front());
 		Q.pop();
 		level.pop();
-		if (old_level != level.front()) printf("\n");
+
+		if (!level.empty() && old_level != level.front() ) {
+			printf("\n\n");
+		}
 	}
+	printf("\n------------------------------\n");
 }
 
 
@@ -90,7 +102,6 @@ node* rec_insert_val(node* root, int x) {
 	return root;
 }
 
-
 bool data_exists(node* root, int x) {
 	bool temp;
 	if (root == NULL) {
@@ -111,6 +122,64 @@ bool data_exists(node* root, int x) {
 	}
 }
 
+node* data_exist_position(node* root, int x){
+	node* temp = root;
+	if (temp == NULL) {
+		return temp;
+	}
+	else if (x == temp->data) {
+		return temp;
+	}
+	else if (x <= temp->data) {
+		temp = data_exist_position(temp->left, x);
+		return temp;
+	}
+	else {
+		temp = data_exist_position(temp->right, x);
+		return temp;
+	}
+}
+node* min_value(node* root) {
+	while (root->left != NULL) {
+		root = root->left;
+	}
+	return root;
+}
+node* max_value(node* root) {
+	while (root->right != NULL) {
+		root = root->right;
+	}
+	return root;
+}
+
+node* data_delete(node* root, int x) {
+	if (root == NULL) return root;
+	else if (x < root->data) root->left = data_delete(root->left,x);
+	else if (x > root->data) root->right = data_delete(root->right,x);
+	else if (root->left==NULL && root->right==NULL){
+		delete root;
+		return NULL;
+	}
+	else if (root->left == NULL || root->right == NULL){
+		if (root->left != NULL) {
+			node* temp = root->left;
+			delete root;
+			return temp;
+		}
+		else {
+			node* temp = root->right;
+			delete root;
+			return temp;
+		}
+	}
+	else if (root->left != NULL && root->right != NULL) {
+		node* temp = min_value(root->right);
+		root->data = temp->data;
+		root->right = data_delete(root->right, temp->data);
+		
+	}
+	return root;
+ }
 
 int main() {
 	bool test_data;
@@ -118,8 +187,16 @@ int main() {
 	root_tree_1= rec_insert_val(root_tree_1,10);
 	root_tree_1 = rec_insert_val(root_tree_1,13);
 	root_tree_1 = rec_insert_val(root_tree_1,8);
-	root_tree_1 = insert_val(root_tree_1, 12);
+	root_tree_1 = rec_insert_val(root_tree_1, 7);
+	root_tree_1 = rec_insert_val(root_tree_1, 12);
+	root_tree_1 = rec_insert_val(root_tree_1, 6);
 	root_tree_1 = rec_insert_val(root_tree_1, 17);
-	test_data = data_exists(root_tree_1, 17);
+	root_tree_1 = rec_insert_val(root_tree_1, 14);
+	root_tree_1 = rec_insert_val(root_tree_1, 18);
 	print_tree_level(root_tree_1);
+	root_tree_1=data_delete(root_tree_1, 13);
+	print_tree_level(root_tree_1);
+	root_tree_1 = data_delete(root_tree_1, 10);
+	print_tree_level(root_tree_1);
+	//printf("%d",root_tree_1->right);
 }
